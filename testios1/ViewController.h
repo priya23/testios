ce9fa8618982e2f67aeb2ed97e99048d1352d840
priyanka353 <priyanka353rocks@gmail.com>
2017-10-31 22:27:27 +0530

    just playing around

--- testios1/ViewController.h
+++ testios1/ViewController.h
@@ -9,7 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+- (IBAction)touchbutton :(id)sender;
+@property (strong,nonatomic) IBOutlet UILabel *label;
 
 @end
 
--- testios1/ViewController.m
+++ testios1/ViewController.m
@@ -25,5 +25,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+- (IBAction)touchbutton :(id)sender{
+    _label.text = @"you are genius";
+}
 @end
